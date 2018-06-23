cd4ffe320c92ec65a2ded984f44ed421c32fba83
guoxiaopeng875 <274642817@qq.com>
2018-06-23 14:25:57 +0800

    登录页面完成

--- app/src/main/java/com/xmkj/md/ui/activity/Login.java
+++ app/src/main/java/com/xmkj/md/ui/activity/Login.java
@@ -1,9 +1,28 @@
 package com.xmkj.md.ui.activity;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import com.xmkj.md.R;
 import com.xmkj.md.base.BaseActivity;
+import com.xmkj.md.utils.AppUtils;
+import com.xmkj.md.utils.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Login extends BaseActivity {
+    @BindView(R.id.iv_back_login)
+    ImageView mIvBackLogin;
+    @BindView(R.id.et_account_login)
+    EditText mEtAccountLogin;
+    @BindView(R.id.et_pwd_login)
+    EditText mEtPwdLogin;
+    @BindView(R.id.btn_login)
+    Button mBtnLogin;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_login;
@@ -16,13 +35,20 @@ public class Login extends BaseActivity {
 
     @Override
     public void initData() {
+    }
 
+    // TODO 调登录接口
+    private void doLogin(String account, String pwd) {
+        ToastUtils.showToast("调登录接口:" + account + "||" + pwd);
     }
 
     @Override
     public void setListener() {
-
+        mBtnLogin.setOnClickListener(view -> {
+            String account = mEtAccountLogin.getText().toString();
+            String pwd = mEtPwdLogin.getText().toString();
+            doLogin(account, pwd);
+        });
+        mIvBackLogin.setOnClickListener(view -> AppUtils.finishActivity(this));
     }
-
-
 }
