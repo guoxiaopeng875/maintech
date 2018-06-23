f88997f46f4e6bf6ec37f5bd500d0a74b690236c
guoxiaopeng875 <274642817@qq.com>
2018-06-23 19:18:11 +0800

    for pull

---
+++ app/src/main/java/com/xmkj/md/ui/activity/QuicklyApply.java
@@ -0,0 +1,28 @@
+package com.xmkj.md.ui.activity;
+
+import com.xmkj.md.R;
+import com.xmkj.md.base.BaseActivity;
+
+/**
+ * 快速报单
+ */
+public class QuicklyApply extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_quickly_apply;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+    }
+
+    @Override
+    public void setListener() {
+    }
+}
