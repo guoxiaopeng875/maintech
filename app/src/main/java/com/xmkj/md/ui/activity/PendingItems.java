fac52cb548aeae866889c2d1b59e1ccb9ee32701
guoxiaopeng875 <274642817@qq.com>
2018-06-24 14:53:54 +0800

    待办事项

--- app/src/main/java/com/xmkj/md/ui/activity/PendingItems.java
+++ app/src/main/java/com/xmkj/md/ui/activity/PendingItems.java
@@ -1,9 +1,27 @@
 package com.xmkj.md.ui.activity;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.xmkj.md.R;
 import com.xmkj.md.base.BaseActivity;
+import com.xmkj.md.model.PendingItemsBean;
+import com.xmkj.md.ui.adapter.PendingItemsAdapter;
+import com.xmkj.md.utils.GenDataUtil;
+import com.xmkj.md.utils.StatusBarSettingUtils;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * 代办事项
+ */
 public class PendingItems extends BaseActivity {
+
+    @BindView(R.id.rv_pending)
+    RecyclerView mRvPending;
 
     @Override
     protected int getLayoutId() {
@@ -12,14 +30,28 @@ public class PendingItems extends BaseActivity {
 
     @Override
     public void initView() {
-
+        StatusBarSettingUtils.setStatusBarColor(this, R.color.toolbar_green);
     }
 
     @Override
     public void initData() {
+        List<PendingItemsBean> pendItems = getPendItems();
+        PendingItemsAdapter pendingItemsAdapter = new PendingItemsAdapter(R.layout.item_pending_view, pendItems);
+        mRvPending.setLayoutManager(new LinearLayoutManager(this));
+        mRvPending.setAdapter(pendingItemsAdapter);
+    }
+
+    // TODO 获取代办事项数据
+    private List<PendingItemsBean> getPendItems() {
+        return GenDataUtil.fakePendingItems();
     }
 
     @Override
     public void setListener() {
+    }
+
+    @OnClick(R.id.ib_back_pending)
+    public void onViewClicked() {
+        finish();
     }
 }
