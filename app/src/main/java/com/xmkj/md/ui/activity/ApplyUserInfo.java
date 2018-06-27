6dfccbc7d8a9bec8a7ed99ec3aa0883fe8970525
guoxiaopeng875 <274642817@qq.com>
2018-06-27 21:58:32 +0800

    fix 客户信息

--- app/src/main/java/com/xmkj/md/ui/activity/ApplyUserInfo.java
+++ app/src/main/java/com/xmkj/md/ui/activity/ApplyUserInfo.java
@@ -1,6 +1,7 @@
 package com.xmkj.md.ui.activity;
 
-import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +13,6 @@ import com.xmkj.md.utils.AppUtils;
 import com.xmkj.md.utils.ToastUtils;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -22,17 +22,19 @@ public class ApplyUserInfo extends BaseActivity {
 
 
     @BindView(R.id.iv_back_user_info)
-    ImageButton mIvBackQuick;
+    ImageButton mIvBackUserInfo;
     @BindView(R.id.btn_cancel_apply_info)
     Button mBtnCancelApplyInfo;
-    @BindView(R.id.et_name_quick)
-    EditText mEtNameQuick;
-    @BindView(R.id.et_cellphone_quick)
-    EditText mEtCellphoneQuick;
-    @BindView(R.id.et_code_quick)
-    EditText mEtCodeQuick;
+    @BindView(R.id.et_name_apply)
+    EditText mEtNameApply;
+    @BindView(R.id.et_cellphone_apply)
+    EditText mEtCellphoneApply;
+    @BindView(R.id.et_customer_name_apply)
+    EditText mEtCustomerNameApply;
     @BindView(R.id.btn_submit_user_info)
-    Button mBtnSubmitQuick;
+    Button mBtnSubmitUserInfo;
+    // 提交按钮是否可以点击
+    private boolean btnClickable = false;
 
     @Override
     protected int getLayoutId() {
@@ -41,6 +43,8 @@ public class ApplyUserInfo extends BaseActivity {
 
     @Override
     public void initView() {
+        mBtnSubmitUserInfo.setBackgroundResource(R.drawable.shape_btn_gray);
+        setBtnClickable();
     }
 
     @Override
@@ -49,6 +53,10 @@ public class ApplyUserInfo extends BaseActivity {
 
     @Override
     public void setListener() {
+        OnTextChanged listener = new OnTextChanged();
+        mEtCellphoneApply.addTextChangedListener(listener);
+        mEtNameApply.addTextChangedListener(listener);
+        mEtCustomerNameApply.addTextChangedListener(listener);
     }
 
     @OnClick({R.id.iv_back_user_info, R.id.btn_cancel_apply_info, R.id.btn_submit_user_info})
@@ -65,4 +73,47 @@ public class ApplyUserInfo extends BaseActivity {
                 break;
         }
     }
+
+    private void setBtnClickable() {
+        mBtnSubmitUserInfo.setClickable(btnClickable);
+    }
+
+    // 改变提交按钮的样式
+    private void changeSubBtn() {
+//        btnClickable ? mBtnSubmitUserInfo.setBackgroundResource(R.drawable.shape_btn_gray) : mBtnSubmitUserInfo.setBackgroundResource(R.drawable.shape_btn_gray);
+    }
+
+    // 返回客户姓名，联系方式， 户主姓名
+    private String[] getInputTexts() {
+        String phone = mEtCellphoneApply.getText().toString();
+        String name = mEtNameApply.getText().toString();
+        String cusName = mEtCustomerNameApply.getText().toString();
+        return new String[]{name, phone, cusName};
+    }
+
+    class OnTextChanged implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String[] texts = getInputTexts();
+            for (String text : texts) {
+                if ("".equals(text)) {
+                    btnClickable = false;
+                    return;
+                }
+            }
+            btnClickable = true;
+        }
+    }
+
 }
