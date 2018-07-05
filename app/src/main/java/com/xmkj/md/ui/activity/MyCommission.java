fae7ace1ea87e5d1440c7a1f8cf25e2203c5fed2
guoxiaopeng875 <274642817@qq.com>
2018-07-05 22:00:59 +0800

    '我的佣金页面完成'

--- app/src/main/java/com/xmkj/md/ui/activity/MyCommission.java
+++ app/src/main/java/com/xmkj/md/ui/activity/MyCommission.java
@@ -1,7 +1,12 @@
 package com.xmkj.md.ui.activity;
 
+import android.view.View;
+
 import com.xmkj.md.R;
 import com.xmkj.md.base.BaseActivity;
+import com.xmkj.md.utils.ToastUtils;
+
+import butterknife.OnClick;
 
 /**
  * 我的佣金
@@ -26,4 +31,21 @@ public class MyCommission extends BaseActivity {
     public void setListener() {
     }
 
+    @OnClick({R.id.toolbar_back_commission, R.id.btn_withdraw, R.id.btn_withdraw_record, R.id.btn_settle_record})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.toolbar_back_commission:
+                finish();
+                break;
+            case R.id.btn_withdraw:
+                ToastUtils.showToast("佣金提现");
+                break;
+            case R.id.btn_withdraw_record:
+                ToastUtils.showToast("提现记录");
+                break;
+            case R.id.btn_settle_record:
+                ToastUtils.showToast("结清记录");
+                break;
+        }
+    }
 }
