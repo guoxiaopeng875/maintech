abb6972971231100789b45ba266aa1464bd7e1e7
guoxiaopeng875 <274642817@qq.com>
2018-07-08 14:37:09 +0800

    绑卡弹窗完成

--- app/src/main/java/com/xmkj/md/ui/activity/MyCommission.java
+++ app/src/main/java/com/xmkj/md/ui/activity/MyCommission.java
@@ -1,10 +1,15 @@
 package com.xmkj.md.ui.activity;
 
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
+import com.example.zhouwei.library.CustomPopWindow;
 import com.xmkj.md.R;
 import com.xmkj.md.base.BaseActivity;
 import com.xmkj.md.utils.AppUtils;
+import com.xmkj.md.utils.PopWinUtil;
 import com.xmkj.md.utils.ToastUtils;
 
 import butterknife.OnClick;
@@ -14,6 +19,8 @@ import butterknife.OnClick;
  */
 public class MyCommission extends BaseActivity {
 
+
+    private CustomPopWindow mCustomPopWindow;
 
     @Override
     protected int getLayoutId() {
@@ -40,7 +47,16 @@ public class MyCommission extends BaseActivity {
                 break;
             case R.id.btn_withdraw:
                 // 佣金提现
-                AppUtils.jump2Next(this, BindCard.class);
+                View popView = View.inflate(this, R.layout.popup_withdraw, null);
+                handlePopupBtn(popView);
+                mCustomPopWindow = new CustomPopWindow.PopupWindowBuilder(this)
+                        .size(ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT)
+                        .setView(popView)
+                        .enableBackgroundDark(true) //弹出popWindow时，背景是否变暗
+                        .setBgDarkAlpha(0.7f) // 控制亮度
+                        .create()
+                        .showAtLocation(view, Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
                 break;
             case R.id.btn_withdraw_record:
                 // 提现记录
@@ -51,5 +67,16 @@ public class MyCommission extends BaseActivity {
                 AppUtils.jump2Next(this, SettleRecords.class);
                 break;
         }
+    }
+
+    // 弹窗按钮点击处理
+    private void handlePopupBtn(View popView) {
+        View.OnClickListener listener = v -> {
+            if (mCustomPopWindow != null) {
+                mCustomPopWindow.dissmiss();
+            }
+            AppUtils.jump2Next(this, BindCard.class);
+        };
+        popView.findViewById(R.id.btn_bind_card_popup).setOnClickListener(listener);
     }
 }
